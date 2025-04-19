04065583d541716bd4b6aac6111bc82e1ec33836
Dina <102176233+DinaEsam@users.noreply.github.com>
2025-04-19 12:39:20 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -46,7 +46,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     //Send image and receive result
 
-    Route::post('/send-image', [FastApiController::class, 'sendImage']);
+   Route::middleware('auth')->post('/send-image', [FastApiController::class, 'sendImage']);
 });
 
 //email
