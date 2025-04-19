819a84cb887c41dc92237b430d9fb9e979e43d38
Dina <102176233+DinaEsam@users.noreply.github.com>
2025-04-19 12:19:30 +0200

    Update FastApiController.php

--- app/Http/Controllers/FastApiController.php
+++ app/Http/Controllers/FastApiController.php
@@ -45,7 +45,7 @@ class FastApiController extends Controller
        // إرسال الصورة لـ FastAPI
        $aiResponse = $this->fastApiService->sendImage($image);
    
-       $user = Auth::check();
+       $user = Auth::user();
        if (!$user) {
            return response()->json(['error' => 'You must log in first'], 403);
        }
