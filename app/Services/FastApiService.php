4052e03a15a1701034c5079e2fed5f27dbcd9763
Dina <102176233+DinaEsam@users.noreply.github.com>
2025-05-16 21:14:19 +0300

    Update FastApiService.php

--- app/Services/FastApiService.php
+++ app/Services/FastApiService.php
@@ -11,7 +11,7 @@ class FastApiService
 
     public function __construct()
     {
-        $this->fastApiUrl = 'https://graduation_project.railway.internal.app/upload-image/';
+        $this->fastApiUrl = 'https://graduationproject-production-8fc3.up.railway.app/upload-image/';
     }
 
     public function sendImage($image)
