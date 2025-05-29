c0d033a67cdd35879aa6071331a66eb68f733264
eyad elwakeel <eelwakeel3@gmail.com>
2025-05-29 18:05:49 +0300

    Update user seeder
    Update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,42 +10,115 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        // Create 2 doctors
+        // Create 4 doctors
         User::create([
-            'name' => 'Dr. John Doe',
-            'email' => 'john.doe@doctor.com',
-            'password' => Hash::make('password123'), // Use a strong password or a placeholder for testing
+            'name' => 'د/مجدي ذكي ',
+            'email' => 'magdy_zaki@doctor.com',
+            'password' => Hash::make('12345678'),
             'role' => 'doctor',
             'national_id' => '123456789',
             'phone' => '0123456789',
         ]);
 
         User::create([
-            'name' => 'Dr. Jane Smith',
-            'email' => 'jane.smith@doctor.com',
-            'password' => Hash::make('password123'),
+            'name' => 'د/راشد زكريا ',
+            'email' => 'rashed_zakria@doctor.com',
+            'password' => Hash::make('12345678'),
             'role' => 'doctor',
             'national_id' => '987654321',
+            'phone' => '0987654321',
+        ]);
+        User::create([
+            'name' => 'د/أحمد صالح ',
+            'email' => 'ahmed_saleh@doctor.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'doctor',
+            'national_id' => '36954321',
+            'phone' => '0987654321',
+        ]);
+        User::create([
+            'name' => 'د/يوسف طه ',
+            'email' => 'yousef_taha@doctor.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'doctor',
+            'national_id' => '145354321',
             'phone' => '0987654321',
         ]);
 
         // Create 2 admins
         User::create([
-            'name' => 'Admin User 1',
-            'email' => 'admin1@admin.com',
-            'password' => Hash::make('password123'),
+            'name' => 'eyad elwakeel',
+            'email' => 'eyad_elwakeel@admin.com',
+            'password' => Hash::make('12345678'),
             'role' => 'admin',
             'national_id' => '111222333',
             'phone' => '01234526789',
         ]);
 
         User::create([
-            'name' => 'Admin User 2',
-            'email' => 'admin2@admin.com',
-            'password' => Hash::make('password123'),
+            'name' => 'dina essam',
+            'email' => 'dina_essam@admin.com',
+            'password' => Hash::make('12345678'),
             'role' => 'admin',
             'national_id' => '444555666',
             'phone' => '0987654387',
+        ]);
+
+        //create 8 patiant
+        User::create([
+            'name' => 'Ahmed Hassan',
+            'email' => 'ahmed.hassan@example.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'patient',
+        ]);
+
+        User::create([
+            'name' => 'Mona Adel',
+            'email' => 'mona.adel@example.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'patient',
+        ]);
+
+        User::create([
+            'name' => 'Youssef Tarek',
+            'email' => 'youssef.tarek@example.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'patient',
+        ]);
+
+        User::create([
+            'name' => 'Nourhan Ahmed',
+            'email' => 'nourhan.ahmed@example.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'patient',
+        ]);
+
+        User::create([
+            'name' => 'Salma Mahmoud',
+            'email' => 'salma.mahmoud@example.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'patient',
+        ]);
+
+        User::create([
+            'name' => 'Karim Nabil',
+            'email' => 'karim.nabil@example.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'patient',
+        ]);
+
+        User::create([
+            'name' => 'Aya Mostafa',
+            'email' => 'aya.mostafa@example.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'patient',
+        ]);
+
+        User::create([
+            'name' => 'Omar Samir',
+            'email' => 'omar.samir@example.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'patient',
         ]);
     }
 }
