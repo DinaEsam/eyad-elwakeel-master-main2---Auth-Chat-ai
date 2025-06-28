1df5814e459203aa5fece40d055f2b5667e2cc9b
Dina <102176233+DinaEsam@users.noreply.github.com>
2025-06-28 18:51:37 +0300

    Create 2025_06_08_183224_create_water_reminders_table.php

---
+++ database/migrations/2025_06_08_183224_create_water_reminders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('water_reminders', function (Blueprint $table) {
+        $table->id();
+        $table->string('type');
+        $table->string('wake_up_time');
+        $table->string('sleep_time');
+        $table->string('reminder_every');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('water_reminders');
+    }
+};
