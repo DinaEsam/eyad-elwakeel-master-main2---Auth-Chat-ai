f0a8a1f9b05a8d7ca0f8da4fc3ffb75c889b0519
Dina <102176233+DinaEsam@users.noreply.github.com>
2025-06-28 18:51:30 +0300

    Create WaterReminder.php

---
+++ app/Models/WaterReminder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WaterReminder extends Model
+{
+   use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'wake_up_time',
+        'sleep_time',
+        'reminder_every',
+    ];}
