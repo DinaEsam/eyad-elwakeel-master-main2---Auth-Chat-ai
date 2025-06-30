2705594f358257f9f0cb8f4a17cb9f1099c6b8b7
Dina <102176233+DinaEsam@users.noreply.github.com>
2025-06-30 15:31:24 +0300

    Update 2025_03_16_193519_create_comments_table.php

--- database/migrations/2025_03_16_193519_create_comments_table.php
+++ database/migrations/2025_03_16_193519_create_comments_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->nullable()->change();
             $table->string('f_name')->nullable();
             $table->string('l_name')->nullable();
             $table->string('email')->nullable();
