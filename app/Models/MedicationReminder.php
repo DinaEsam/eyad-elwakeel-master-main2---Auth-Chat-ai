f0d06afc37ed6db149518bdfb2b6d91299b4c830
Dina <102176233+DinaEsam@users.noreply.github.com>
2025-07-03 20:47:33 +0300

    Update MedicationReminder.php

--- app/Models/MedicationReminder.php
+++ app/Models/MedicationReminder.php
@@ -10,7 +10,7 @@ class MedicationReminder extends Model
   use HasFactory;
 
     protected $fillable = [
-      
+        'user_id',
         'medicine_name',
         'dose_count',
         'period',
